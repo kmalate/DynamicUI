ac546a99f523c7710218c452da53e655b3516bfc
Khalid Malate <khalid.malate@gmail.com>
2016-02-08 14:08:52 +0800

    Building Flexible UI

--- app/src/main/java/com/example/khalid/dynamicui/MainActivity.java
+++ app/src/main/java/com/example/khalid/dynamicui/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.khalid.dynamicui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +35,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void openFlexibleActivity(View view) {
+        Intent intent = new Intent(this, FlexibleActivity.class);
+        startActivity(intent);
     }
 }
